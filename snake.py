7ca614a2ce8c86ce985424143385c4a64da407b3
daniela <moreno.tobar.daniela@gmail.com>
2022-04-26 12:51:46 -0500

    positions

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle 
+import time
 
 #Crea el escenario del evento 
 screen = Screen() #Instanciamos el objeto 
@@ -6,17 +7,47 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+screen.tracer(0) #quitamos animación de movimiento
 
 
 #Creación del cuerpo de la serpiente 
 
-positions = [(0,0),(-20, 0), (-40, 0)]
+starting_positions = [(0,0),(-20, 0), (-40, 0)] #Almacenando posiciones
+
+segments = []
 
 #Por cada posicion en position se va a ejecutar el recorrido de positions
-for position in positions: 
+for position in starting_positions: 
     snake_segment = Turtle("square")
     snake_segment.color("white")
+    snake_segment.penup()
     snake_segment.goto(position)
+    segments.append(snake_segment)
+    
+
+#Establecer estado del juego
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+
+
+    '''
+    for segment in segments:
+        segment.forward(20)
+        segment[0].left(90)
+    '''
+
+
+
+    for seg_num in range(len(segments) - 1, 0, - 1): #Quiero que me almacene todas las nuevas posiciones
+        new_x = segments[seg_num - 1].xcor() #cor = coordenada x coordenada y
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y) #movimiento segun las coordenadas que tengo, y aquí debo indicarle donde debe hacer el movimiento, por ende necesita saber la posicion de los cuadritos.
+
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 '''
